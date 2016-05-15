487fcc8fdd432e60d6b4ae8efe51ee5a6dfcbe1a
Vladimir Mitrovic <v@brutalism.rs>
2016-05-15 23:28:09 +0200

    Apply PTF to field walk

--- src/bru_9/scenes/core.cljs
+++ src/bru_9/scenes/core.cljs
@@ -12,7 +12,7 @@
             [thi.ng.math.core :as m]))
 
 (def config
-  {:wasd-speed 0.5
+  {:wasd-speed 0.05
    :mouse-sensitivity 0.4})
 
 ;; Each sketch has a couple of hooks that tie into the main loop defined in this
--- src/bru_9/scenes/tptf.cljs
+++ src/bru_9/scenes/tptf.cljs
@@ -1,28 +1,47 @@
 (ns bru-9.scenes.tptf
-  (:require [bru-9.util :as u]
-            [bru-9.color.core :as c]
+  (:require [bru-9.color.core :as c]
             [bru-9.color.infinite :as ci]
+            [bru-9.field.core :as f]
+            [bru-9.field.linear :as fl]
             [bru-9.interop :as i]
+            [bru-9.util :as u]
             [thi.ng.geom.attribs :as attr]
             [thi.ng.geom.core :as g]
             [thi.ng.geom.vector :as v]
             [thi.ng.geom.ptf :as ptf]
             [thi.ng.geom.circle :as circle]
-            [thi.ng.geom.webgl.glmesh :as glm]))
+            [thi.ng.geom.webgl.glmesh :as glm]
+            [thi.ng.math.core :as m]))
 
-(def config {:elements 200})
+(def config {:elements 20
+             :walk-length 32
+             :walk-multiplier 1.2
+             :circle-resolution 16
+             :field-dimension 20})
+
+(defonce field
+  (let [dim (:field-dimension config)]
+    (fl/linear-field [dim dim dim] #(v/randvec3))))
+
+(defn random-offsets [n m]
+  (take n (repeatedly #(v/randvec3 m))))
+
+(defonce offsets (random-offsets (:walk-length config) 0.2))
 
 (defn element [acc color]
-  (let []
-    (-> (take 3 (repeatedly #(v/randvec3 (rand 10))))
+  (let [hops (:walk-length config)
+        cr (:circle-resolution config)
+        mul (:walk-multiplier config)
+        hres (/ (:field-dimension config) 2)]
+    (-> (map #(m/+ %1 %2)
+             (f/walk field (m/+ (v/vec3 hres hres hres) (v/randvec3)) hops mul)
+             offsets)
         (ptf/sweep-mesh
-         (g/vertices (circle/circle 0.1) 5)
+         (g/vertices (circle/circle 0.05) cr)
          {:mesh acc
           :attribs {:col (-> color (repeat) (attr/const-face-attribs))}}))))
 
 ;; Scene setup & main loop
-
-(defonce *state* (atom {}))
 
 (defn setup [initial-context]
   (let [scene (:scene initial-context)
@@ -33,7 +52,6 @@
         mesh (reduce element acc (take elem-count palette))
         three-mesh (i/three-mesh mesh)]
     (.add scene three-mesh)
-    (reset! *state* (assoc @*state* :scene scene))
     initial-context))
 
 (defn reload [context]
