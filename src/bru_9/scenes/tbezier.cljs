221173a2a674b456e256c633cd43cf916d4dcb69
Vladimir Mitrovic <v@brutalism.rs>
2016-05-22 17:49:15 +0200

    Aesthetic starting to form in tbezier.cljs

--- src/bru_9/scenes/tbezier.cljs
+++ src/bru_9/scenes/tbezier.cljs
@@ -10,13 +10,13 @@
             [thi.ng.geom.vector :as v]
             [thi.ng.math.core :as m]))
 
-;; Scene setup & main loop
+(defn- field-generator [coords intensity direction]
+  (m/+ direction (v/randvec3 intensity)))
 
-(def config {:num-splines 40
-             :num-hops 10
+(def config {:num-splines 100
+             :num-hops 4
              :offset-radius 0.2
-             :field (fl/linear-field [5 5 5] #(v/randvec3))
-             :center (v/vec3 2.5 2.5 2.5)})
+             :field (fl/linear-field [5 5 5] #(field-generator % 1.0 v/V3Z))})
 
 (defn- spline-walk
   "Returns a bezier spline resulting from walking a given field from the given
@@ -25,7 +25,7 @@
   (b/auto-spline3 (f/walk field startpos hops)))
 
 (defn setup [initial-context]
-  (let [{:keys [num-splines num-hops offset-radius center field]} config
+  (let [{:keys [num-splines num-hops offset-radius field]} config
         zdim (last (f/dimensions field))
         zcenters (range 0 zdim (/ zdim num-splines))
         centers (map #(m/+ (v/vec3 0 0 %) (v/randvec3 offset-radius)) zcenters)
