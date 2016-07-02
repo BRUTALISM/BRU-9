5928fbda3e03b4fbb1fe8b160e8190329c094f31
Vladimir Mitrovic <v@brutalism.rs>
2016-07-02 13:12:50 +0200

    Configurable bezier curve tightness and multiplication function params

--- src/bru_9/geom/bezier.cljs
+++ src/bru_9/geom/bezier.cljs
@@ -1,12 +1,20 @@
 (ns bru-9.geom.bezier
-  (:require [thi.ng.geom.bezier :as b]
-            [bru-9.field.core :as f]))
+  (:require [bru-9.field.core :as f]
+            [thi.ng.geom.bezier :as b]
+            [thi.ng.geom.vector :as v]))
 
-(defn- spline-walk
+(defn auto-spline3
+  [points tight]
+  (->> points
+       (b/find-cpoints* v/vec3 tight)
+       (b/auto-spline* points)
+       (thi.ng.geom.types.Bezier3.)))
+
+(defn spline-walk
   "Returns a bezier spline resulting from walking a given field from the given
   start position, using the given number of hops. The mulfn parameter is a
   multiplication function passed to field.core/walk, see the docs there for
   details."
-  [field startpos hops mulfn]
-  (b/auto-spline3 (f/walk field startpos hops mulfn)))
+  [field startpos hops mulfn tight]
+  (auto-spline3 (f/walk field startpos hops mulfn) tight))
 
